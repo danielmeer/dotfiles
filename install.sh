a7063a16a7fb9c242deacf8d621956131d1e57fe
Daniel Meer <meerdan2@gmail.com>
2018-01-13 20:44:26 +0100

    Improve install script formatting

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@
 # Variables
 DOT_DIR=~/.dotfiles
 OLD_DIR=~/dotfiles_old
-FILES="bash/bashrc bash/bash_profile"  # Not used at the moment
 
 # Create backup dir
 echo -n "Creating $OLD_DIR for backup of any existing dotfiles ..."
@@ -15,11 +14,11 @@ echo "done"
 
 # Move existing files to backup dir, then create symlinks
 echo "Moving dotfiles to $OLD_DIR"
-mv ~/.bashrc $OLD_DIR/
+mv ~/.bashrc       $OLD_DIR/
 mv ~/.bash_profile $OLD_DIR/
-mv ~/.zshrc $OLD_DIR/
+mv ~/.zshrc        $OLD_DIR/
 
 echo "Creating symlinks"
-ln -s $DOT_DIR/bash/bashrc ~/.bashrc
+ln -s $DOT_DIR/bash/bashrc       ~/.bashrc
 ln -s $DOT_DIR/bash/bash_profile ~/.bash_profile
-ln -s $DOT_DIR/zsh/zshrc ~/.zshrc
+ln -s $DOT_DIR/zsh/zshrc         ~/.zshrc
