50bf454a93acb9759d3ccdd5cfdf5964adbb6fd8
Daniel Meer <daniel.meer@speedgoat.ch>
2023-07-28 08:54:50 +0200

    Convert fish config to be used on Linux

--- fish/config.fish
+++ fish/config.fish
@@ -8,22 +8,23 @@
 #export TIME_STYLE=long-iso
 
 # Default editor
-#export EDITOR='nvim'
-#export VISUAL='nvim'
+export EDITOR='nvim'
+export VISUAL='nvim'
 
+# MATLAB cannot use fish because its libstdc++ is too old
+set -gx MATLAB_SHELL /bin/bash
 
 # Aliases
 alias ls='ls --color=auto --group-directories-first'
 #alias vim='nvim'
 
-function g --wraps=git
-    /mnt/c/Program\ Files/Git/bin/git.exe $argv
-end
-alias gd='TMPDIR=tmp g difftool'
-alias gitmerge='TMPDIR=tmp g mergetool --tool=meld'
-#alias gdm='TMPDIR=tmp git difftool --tool=matlab'
-#alias gitmergem='TMPDIR=tmp git mergetool --tool=matlab'
-alias gdbc='TMPDIR=tmp g difftool --tool=bc'
+alias t='tig --all'
+alias g='git status'
+alias gd='git difftool'
+alias gitmerge='git mergetool --tool=meld'
+#alias gdm='git difftool --tool=matlab'
+#alias gitmergem='git mergetool --tool=matlab'
+alias gdbc='git difftool --tool=bc'
 #alias clearoxygen='\
 #    git checkout thirdpartydrivers/sg_help/*; \
 #    git checkout user_docs/generated_pdfs/*; \
@@ -33,11 +34,9 @@ alias gdbc='TMPDIR=tmp g difftool --tool=bc'
 #alias slxdiff='/mnt/c/Users/daniel.meer/OneDrive\ -\ Speedgoat\ GmbH/Files/scripts/slx-diff.sh'
 #alias gdslx='TMPDIR=tmp git difftool --tool=slx'
 #
-#alias gs='git status'
-#
-function clean
-    /mnt/c/Users/daniel.meer/OneDrive\ -\ Speedgoat\ GmbH/Files/scripts/cleanup-whitespace.sh $argv
-end
+#function clean
+#    /mnt/c/Users/daniel.meer/OneDrive\ -\ Speedgoat\ GmbH/Files/scripts/cleanup-whitespace.sh $argv
+#end
 #gclean() {
 #    clean $(git diff-tree --diff-filter=d --no-commit-id --name-only -r $1)
 #}
@@ -48,9 +47,6 @@ end
 #        exec /usr/bin/git "$@"
 #    fi
 #}
-
-#alias wrike-time='python3 /mnt/c/Users/daniel.meer/OneDrive\ -\ Speedgoat\ GmbH/Files/scripts/wrike-time.py'
-
 
 # Paths for TeX Live
 #if [ -d "${HOME}/opt/texlive/2016" ]; then
@@ -68,3 +64,13 @@ end
 #if which tmux 2>&1 > /dev/null; then
 #	test -z "$TMUX" && (tmux attach || tmux new-session)
 #fi
+
+# Load foreign-env plugin
+set fish_function_path $fish_function_path ~/opt/plugin-foreign-env/functions
+
+# For Nix home-manager
+fenv source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
